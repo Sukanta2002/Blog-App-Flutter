f105fd960f18da85cc3ce8580e5ad3da0abf9ad9
sukanta2002 <sukantabehera096@gmail.com>
2024-03-28 19:51:40 +0530

    Supabase Connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:blog_app/core/router/app_router_config.dart';
+import 'package:blog_app/core/secrets/supabase_secrets.dart';
 import 'package:blog_app/core/theme/theme.dart';
 
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: SuperbaseSecrets.supabaseUrl,
+    anonKey: SuperbaseSecrets.anonKey,
+  );
   runApp(const MyApp());
 }
 
